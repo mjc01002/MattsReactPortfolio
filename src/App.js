59f07e3939493239a5dd47285ec488f19836fe51
stoncalkins@gmail.com <stoncalkins@gmail.com>
2022-02-19 13:26:58 -0500

    updated cover photo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Header from './components/header';
 import Footer from './components/footer';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import cover from './assets/cover/portfolioImage.jpg';
+
 
 function App() {
   return (
     <div>
       
       <Header />
+      <img src={cover} alt="cover"/>
       <Footer/>
   
     </div>
