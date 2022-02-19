7dc0cc32c24989df95330d233d81b4f96dad6a0a
stoncalkins@gmail.com <stoncalkins@gmail.com>
2022-02-19 13:14:36 -0500

    updated homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/header';
 import Footer from './components/footer';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div>
