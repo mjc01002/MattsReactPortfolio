e846c8e86ce398422a869b4e3c59d6dffef0240a
stoncalkins@gmail.com <stoncalkins@gmail.com>
2022-02-19 12:31:35 -0500

    updated to have header and footer show up

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Nav from "./components/Nav";
-import AboutMe from "./components/aboutMe";
-import Portfolio from "./components/portfolio";
-import Contact from './components/contact';
-import Resume from './components/resume';
+import Header from './components/header';
+import Footer from './components/footer';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 function App() {
-      return (
-          <BrowserRouter>
-          <header className="navcustom">
-            <Nav />
-          </header>
-          
-          <div className="content bodycustom">
-              <Routes>
-            <Route exact path="/" render={() => <Navigate to="/aboutMe" />}/>
-            <Route path="/aboutMe" element={<AboutMe />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />}/>
-            <Route path="/resume" element={<Resume />}/>
-              </Routes>
-          </div>
+  return (
+    <div>
+      
+      <Header />
+      <Footer/>
   
-          </BrowserRouter>
-        
-      );
-    }
-
+    </div>
+  );
+}
 
 export default App;
